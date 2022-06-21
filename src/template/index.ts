40560c10c5779b72bab63f21da3a3c80d2da426d
mochaazul <m.zulfikar@rebelworks.co>
2022-06-21 16:29:44 +0700

    [Cleanup] Restructure folder

--- src/template/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './store'
-export * from './action'
-export * from './reducer'
-export * from './root'
--- src/template/root.ts
+++
@@ -1,6 +0,0 @@
-export const generateIndex = () => {
-    return `export * from './action'
-    export * from './reducer'
-    export * from './model'
-    `
-}
